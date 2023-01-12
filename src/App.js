3734d311bcdcf33f1906ac0c5bdba25ffecd1846
Thangril <50687122+Thangril@users.noreply.github.com>
2023-01-12 17:04:12 +0900

    Added Directory component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import './categories.styles.scss';
+import Directory from './components/directory/directory.component';
 
 const App = () => {
 
@@ -30,22 +29,9 @@ const App = () => {
       imageUrl:'https://i.ibb.co/R70vBrQ/men.png',
     },
   ]
-  return (
-    <div className="categories-container">
-      {categories.map(( {title, id, imageUrl} ) => (
-          <div key={id} className="category-container">
-            <div className='background-image' style={{
-                backgroundImage: `url(${imageUrl})`
-              }} />        
-            <div className="category-body-container">
-              <h2>{title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
-      ))}
-
-    </div>     
-  );
-}
+  return  <Directory categories={categories} />   
+   
+  
+};
 
 export default App;
