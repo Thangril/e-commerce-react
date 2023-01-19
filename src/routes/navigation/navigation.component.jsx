a8d3be3838ec573da6c67c526f5d7c1abb14c13b
Thangril <50687122+Thangril@users.noreply.github.com>
2023-01-19 21:45:21 +0900

    added Toggle Cart Open

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,13 +2,17 @@ import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg'; 
 import { UserContext } from '../../context/user.context';
+import { CartContext } from '../../context/cart.context';
 import { signOutUser } from '../../utilts/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropDown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import './navigation.styles.scss';
 
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
   
   return (
     <Fragment>
@@ -26,8 +30,10 @@ const Navigation = () => {
             <Link className='nav-link' to='/auth'>
               SIGN IN
             </Link>
-          )}          
+          )}
+          <CartIcon/>          
         </div>
+        {isCartOpen && <CartDropDown />}        
       </div>    
       <Outlet />
     </Fragment>    
