3203dfc27f380b8782d635b71588e5e3e7115e55
Thangril <50687122+Thangril@users.noreply.github.com>
2023-01-20 22:16:28 +0900

    Added checkout component

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,15 +1,71 @@
-import { createContext, useState} from "react";
+import { createContext, useState, useEffect} from "react";
 
-import PRODUCTS from '../shop-data.json';
+const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find(
+  (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  if(existingCartItem) {
+    return cartItems.map((cartItem) => cartItem.id === productToAdd.id 
+    ? {...cartItem, quantity: cartItem.quantity + 1}
+    : cartItem
+    );
+  }
+
+  return [...cartItems, { ...productToAdd, quantity: 1}];
+};
+
+const removeCartItem = (cartItems, cartItemToRemove)=> {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+    );
+
+    if(existingCartItem.quantity === 1) {
+      return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+    return cartItems.map((cartItem) => cartItem.id === cartItemToRemove.id 
+    ? {...cartItem, quantity: cartItem.quantity - 1}
+    : cartItem
+    );
+}
+
 
 export const CartContext = createContext({
   isCartOpen: false,
-  setIsCartOpen: () => {}
-})
+  setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  removeItemFromCart: () => {},
+  cartCount: 0
+});
 
 export const CartProvider =  ({children}) =>{
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = {isCartOpen, setIsCartOpen};
+  const [cartItems, SetCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,0)
+      setCartCount(newCartCount);
+  },[cartItems]);
+
+  const addItemToCart = (productToAdd) => {
+    SetCartItems(addCartItem(cartItems, productToAdd));
+  }
+
+  const removeItemFromCart = (cartItemToRemove) => {
+    SetCartItems(removeCartItem(cartItems, cartItemToRemove));
+  }
+
+  const value = {
+    isCartOpen, 
+    setIsCartOpen, 
+    addItemToCart,
+    cartItems, 
+    cartCount,
+    removeItemFromCart
+   };
   
   return (
    <CartContext.Provider value={value}>
---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,30 @@
+import { useContext } from 'react';
+import { CartContext } from '../../context/cart.context';
+import { CartItem } from '../../components/cart-item/cart-item.component';
+import './checkout.styles.scss';
+
+const Checkout = () => {
+
+  const { cartItems, addItemToCart, removeItemFromCart } = useContext(CartContext);
+
+  return (
+    <div>
+      <div>
+        {cartItems.map((cartItem) => {
+          const { id, name, quantity } = cartItem;
+          return ( 
+          <div key={id}>
+            <h2>{name}</h2>
+            <span>{quantity}</span>
+            <span onClick={() =>removeItemFromCart(cartItem)}>decrement</span>
+            <span onClick={() =>addItemToCart(cartItem)}>increment</span>
+          </div>
+          )
+        })}          
+        </div>
+    </div>
+  
+  );
+};
+
+export default Checkout
